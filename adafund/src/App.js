3eff20ce504b547c8c747e9d4503e7e5e1cd311a
giorgos208 <75020342+giorgos208@users.noreply.github.com>
2023-03-27 22:58:49 +0300

    progress

--- adafund/src/App.js
+++ adafund/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Requests from './components/Requests';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import GlobalStyles from './components/GlobalStyles';
 import background from './components/background.png';
-//import BackgroundImage from './components/BackgroundImage';
 import styled from 'styled-components';
 
 const MainContent = styled.div`
@@ -16,16 +16,17 @@ const MainContent = styled.div`
   flex-direction: column;
   justify-content: space-between;
 `;
+
 const ImageContainer = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
   z-index: -1;
   background-image: url(${background});
-
   background-position: center;
+  background-attachment: scroll;
   background-repeat: no-repeat;
   background-size: cover;
   opacity: 0.3; /* optional: adjust the opacity as needed */
@@ -42,6 +43,7 @@ function App() {
           <Route path="/" element={<Hero />} />
           <Route path="/features" element={<Features />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/requests" element={<Requests />} />
           {/* Add more routes as needed */}
         </Routes>
         <Footer />
@@ -49,7 +51,5 @@ function App() {
     </Router>
   );
 }
-
-
 
 export default App;
